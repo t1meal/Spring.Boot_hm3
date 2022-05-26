93b486f224757d42cfba08d08553c1f83bfe8d77
Алексей <t1meal@yandex.ru>
2022-05-26 19:48:22 +0300

    Initial commit

---
+++ src/main/java/ru/gb/webapp/SpringHm3Application.java
@@ -0,0 +1,13 @@
+package ru.gb.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHm3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHm3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/webapp/SpringHm3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHm3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
